0716620bd82e415b0a91d2f3d6074efd8c8093a4
adevvvv <adevvv@bk.ru>
2026-01-05 16:26:07 +0300

    feat: add ImagingSatellite implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Тестирование спутника ДЗЗ:");
+        ImagingSatellite imgSat = new ImagingSatellite("ДЗЗ-Тест", 0.9, 2.5);
+
+        System.out.println("Активация спутника:");
+        boolean activated = imgSat.activate();
+        System.out.println("Активация " + (activated ? "успешна" : "не удалась"));
+
+        if (activated) {
+            System.out.println("\nВыполнение миссии:");
+            imgSat.performMission();
+        }
+
+        System.out.println("\nСостояние спутника: " + imgSat.toString());
     }
 }
